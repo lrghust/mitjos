c78ebcf9e183ea05abc9a471240695d58d427ea3
Ruiguang Li <lrghust@gmail.com>
2018-03-06 17:07:19 +0800

    lab4-exercise14 finish

--- kern/trap.c
+++ kern/trap.c
@@ -99,26 +99,26 @@ trap_init(void)
 	extern struct Segdesc gdt[];
 
 	// LAB 3: Your code here.
-	SETGATE(idt[T_DIVIDE], 1, GD_KT, divide_error_handler, 0);
-	SETGATE(idt[T_DEBUG], 1, GD_KT, debug_exception_handler, 0);
-	SETGATE(idt[T_NMI], 1, GD_KT, non_maskable_interrupt_handler, 0);
-	SETGATE(idt[T_BRKPT], 1, GD_KT, breakpoint_handler, 3);
-	SETGATE(idt[T_OFLOW], 1, GD_KT, overflow_handler, 0);
-	SETGATE(idt[T_BOUND], 1, GD_KT, bounds_check_handler, 0);
-	SETGATE(idt[T_ILLOP], 1, GD_KT, illegal_opcode_handler, 0);
-	SETGATE(idt[T_DEVICE], 1, GD_KT, device_not_available_handler, 0);
-	SETGATE(idt[T_DBLFLT], 1, GD_KT, double_fault_handler, 0);
-	SETGATE(idt[T_TSS], 1, GD_KT, invalid_task_switch_segment_handler, 0);
-	SETGATE(idt[T_SEGNP], 1, GD_KT, segment_not_present_handler, 0);
-	SETGATE(idt[T_STACK], 1, GD_KT, stack_exception_handler, 0);
-	SETGATE(idt[T_GPFLT], 1, GD_KT, general_protection_fault_handler, 0);
-	SETGATE(idt[T_PGFLT], 1, GD_KT, page_fault_kern_handler, 0);
-	SETGATE(idt[T_FPERR], 1, GD_KT, floating_point_error_handler, 0);
-	SETGATE(idt[T_ALIGN], 1, GD_KT, aligment_check_handler, 0);
-	SETGATE(idt[T_MCHK], 1, GD_KT, machine_check_handler, 0);
-	SETGATE(idt[T_SIMDERR], 1, GD_KT, simd_floating_point_error_handler, 0);
+	SETGATE(idt[T_DIVIDE], 0, GD_KT, divide_error_handler, 0);
+	SETGATE(idt[T_DEBUG], 0, GD_KT, debug_exception_handler, 0);
+	SETGATE(idt[T_NMI], 0, GD_KT, non_maskable_interrupt_handler, 0);
+	SETGATE(idt[T_BRKPT], 0, GD_KT, breakpoint_handler, 3);
+	SETGATE(idt[T_OFLOW], 0, GD_KT, overflow_handler, 0);
+	SETGATE(idt[T_BOUND], 0, GD_KT, bounds_check_handler, 0);
+	SETGATE(idt[T_ILLOP], 0, GD_KT, illegal_opcode_handler, 0);
+	SETGATE(idt[T_DEVICE], 0, GD_KT, device_not_available_handler, 0);
+	SETGATE(idt[T_DBLFLT], 0, GD_KT, double_fault_handler, 0);
+	SETGATE(idt[T_TSS], 0, GD_KT, invalid_task_switch_segment_handler, 0);
+	SETGATE(idt[T_SEGNP], 0, GD_KT, segment_not_present_handler, 0);
+	SETGATE(idt[T_STACK], 0, GD_KT, stack_exception_handler, 0);
+	SETGATE(idt[T_GPFLT], 0, GD_KT, general_protection_fault_handler, 0);
+	SETGATE(idt[T_PGFLT], 0, GD_KT, page_fault_kern_handler, 0);
+	SETGATE(idt[T_FPERR], 0, GD_KT, floating_point_error_handler, 0);
+	SETGATE(idt[T_ALIGN], 0, GD_KT, aligment_check_handler, 0);
+	SETGATE(idt[T_MCHK], 0, GD_KT, machine_check_handler, 0);
+	SETGATE(idt[T_SIMDERR], 0, GD_KT, simd_floating_point_error_handler, 0);
 
-	SETGATE(idt[T_SYSCALL], 1, GD_KT, system_call_handler, 3);
+	SETGATE(idt[T_SYSCALL], 0, GD_KT, system_call_handler, 3);
 
 	SETGATE(idt[IRQ_OFFSET + IRQ_TIMER], 0, GD_KT, timer_handler, 0);
 	SETGATE(idt[IRQ_OFFSET + IRQ_KBD], 0, GD_KT, kbd_handler, 0);
@@ -247,6 +247,11 @@ trap_dispatch(struct Trapframe *tf)
 		// Handle clock interrupts. Don't forget to acknowledge the
 		// interrupt using lapic_eoi() before calling the scheduler!
 		// LAB 4: Your code here.
+		case IRQ_OFFSET + IRQ_TIMER:{
+			lapic_eoi();
+			sched_yield();
+			break;
+		}
 
 		case T_PGFLT:{
 			page_fault_handler(tf);
